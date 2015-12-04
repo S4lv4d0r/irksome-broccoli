4431a2beb356b20111230528a5c5f1368c662ba8
S4lv4d0r <yosoyvoro@hotmail.com>
2015-12-04 10:54:24 +0100

    SMS recibido

--- Servidor/Servidor/Program.cs
+++ Servidor/Servidor/Program.cs
@@ -20,6 +20,7 @@ namespace Servidor
             IPEndPoint destino = new IPEndPoint(IPAddress.Parse("192.168.1.223"),5555);
             //definimos el socket
             Socket servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            byte[] bufferEscritura = null;
             try
             {
                 //asocioamos el socket a la direccion de destino
@@ -28,6 +29,12 @@ namespace Servidor
                 servidor.Listen(1);
                 Console.WriteLine("Escuchando...");
                 Socket escuchar = servidor.Accept();
+                Console.WriteLine("Conexion con exito");
+                bufferEscritura = new byte[255];
+                int a = escuchar.Receive(bufferEscritura, 0, bufferEscritura.Length, 0);
+                Array.Resize(ref bufferEscritura, a);
+                Console.WriteLine("El cliente dice: "+Encoding.Default.GetString(bufferEscritura));
+                servidor.Close();
                 /*
                  *creamos un nuevo socket en cuanto recibe peticion de conexion, asi 
                  *nuestro servidor queda disponible para seguir escuchando a otros 
